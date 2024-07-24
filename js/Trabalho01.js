27855435eb1248299aaf6dd10cbb16a2fc690217
Maeda-Hiro <hhmlnm@gmail.com>
2024-07-23 22:17:43 -0300

    correcao chamadas e tabs

--- js/Trabalho01.js
+++ js/Trabalho01.js
@@ -1,31 +1,36 @@
-$(document).ready(function(){
-  ConfiguraTabs();
-  Eventos();
+$(document).ready(function () {
+	ConfiguraTabs();
+	Eventos();
 });
-  
-function ConfiguraTabs(){
-  $(".nav-tabs a").click(function(){
-    $(this).tab('show');
-  });
-  $('.nav-tabs a').on('shown.bs.tab', function(event){
-    var x = $(event.target).text();
-    var y = $(event.relatedTarget).text();-
-    $(".act span").text(x);
-    $(".prev span").text(y);
-  });
+
+function ConfiguraTabs() {
+	$(".nav-tabs a").click(function () {
+		$(this).tab('show');
+	});
+	$('.nav-tabs a').on('shown.bs.tab', function (event) {
+		var x = $(event.target).text();
+		var y = $(event.relatedTarget).text(); -
+			$(".act span").text(x);
+		$(".prev span").text(y);
+	});
 };
 
-function Eventos(){
-  $("#botaoDeCadastro").on('click', function () {
-      console.log("Está chegando até aqui")
-      $("#Titulo").hide();
-      window.open('./Cadastro.html' , '_blank');
-  })
+function Eventos() {
+	$("#botaoDeCadastro").on('click', function () {
+		console.log("Está chegando até aqui")
+		window.open('./Cadastro.html', '_self');
+		// $("#Titulo").hide();
+	})
 
-  $("#cadUsuForm_Enviar").on('click', function () {
-      // console.log("Está chegando até aqui")
-      // window.open('./Cadastro.html' , '_self');
-  })
+	$("#cadUsuForm_btProx").on('click', function () {
+		$('#Cadastros a[href="#cadPag"]').tab('show')
+	})
+	$("#cadUsuForm_btVoltar").on('click', function () {
+		$('#Cadastros a[href="#cadUsu"]').tab('show')
+	})
+	$("#cadUsuForm_btProx").on('click', function () {
+		// $('#Cadastros a[href="#cadPag"]').tab('show')
+	})
 }
 
 // .LinksNaoPossuiCadastro{
